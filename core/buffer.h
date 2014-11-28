5f9d1cdbb333fd64fddb47585878a550d846b282
Sergey Khoroshavin <skhoroshavin@gmail.com>
2014-11-28 09:26:15 +0300

    More safety in buffer operations

--- core/buffer.h
+++ core/buffer.h
@@ -37,6 +37,8 @@
  * Buffer modification                                                 *
 \***********************************************************************/
 
-#define buf_push_back(buf,value) do { (buf)->data[(buf)->size] = value; ++(buf)->size; } while(0)
-#define buf_pop_back(buf) do { --(buf)->size; } while(0)
+#define buf_push_back_unsafe(buf,value) do { (buf)->data[(buf)->size] = value; ++(buf)->size; } while(0)
+#define buf_pop_back_unsafe(buf) do { --(buf)->size; } while(0)
+#define buf_push_back(buf,value) do { if( !buf_is_full(buf) ) buf_push_back_unsafe(buf,value); } while(0)
+#define buf_pop_back(buf) do { if( !buf_is_empty(buf) ) buf_pop_back_unsafe(buf); } while(0)
 #define buf_clear(buf) do { (buf)->size = 0; } while(0)
